3d6084a545e04bb749b8a85681cd72cbbab79265
Pooja dev <poojahosabettu@gmail.com>
2021-05-15 16:14:18 +0530

    Time: 0 ms (100.00%), Space: 37.3 MB (74.73%) - LeetHub

--- sort-colors/sort-colors.java
+++ sort-colors/sort-colors.java
@@ -6,21 +6,23 @@ class Solution {
         int mid = 0;
         
         while(mid<=high){
-            int temp = 0;
-            if(nums[mid] == 2){
-                temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high = high-1;
-            }else if(nums[mid] == 0){
-                temp = nums[mid];
-                nums[mid] = nums[low];
-                nums[low] = temp;
-                low = low+1;
-                ++mid;
+            if(nums[mid] == 1)
+                mid = mid+1;
+            else {
+                int temp = 0;
+                if(nums[mid] == 2){
+                    temp = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp;
+                    high = high-1;
+                }else{
+                    temp = nums[mid];
+                    nums[mid] = nums[low];
+                    nums[low] = temp;
+                    low = low+1;
+                    ++mid;
+                }
             }
-            else
-                ++mid;
         }
     }
 }
